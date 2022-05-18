750dd2dcf9224b8739119fde35a80d2d83c649c7
Lucas Gomes <lucas.gomes.manaus@gmail.com>
2022-05-17 22:09:10 -0400

    Quest de Js Basico

---
+++ Quest-JS - Basico/script.js
@@ -0,0 +1,28 @@
+
+const valor1 = 3
+const valor2  = 5
+
+console.log(somar(valor1,valor2))
+console.log(subtracao(valor1,valor2))
+console.log(multiplicacao(valor1,valor2))
+console.log(divisao(valor1,valor2))
+
+
+
+function somar(a,b){
+    return'A soma entre esses numeros é '+ (valor1+valor2)
+}
+function subtracao(a,b){
+    return'A subtração entre esses numeros é '+ (valor1-valor2);
+}
+function multiplicacao(a,b){
+    return'O produto entre esses numeros é '+ (valor1+valor2)
+}
+function divisao(a,b){
+    return'A divisao entre esses numeros é '+ (valor1/valor2)
+}
+
+
+
+
+
